11fb12f24245150c779820c437944a7b3841a1db
rashmi <rashmi8shahi@gmail.com>
2024-12-10 16:43:54 +0530

    initial commit

---
+++ classroom/routes/post.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+//Posts
+//Index 
+router.get("/", (req,res)=>{
+    res.send("GET for posts")
+});
+
+//Show
+router.get("/:id", (req,res)=>{
+    res.send("GET for posts ID");
+})
+
+//POST 
+router.post("/", (req,res)=>{
+    res.send("POST for posts")
+})
+
+//DELETE
+router.delete("/:id",(req,res)=>{
+    res.send("DELETE for posts id")
+})
+
+module.exports = router;
